1e5ae684eee052d82e56b77bb55a68d75f42f4b9
Kim Jeker <github@kije.ch>
2016-07-22 14:21:22 +0200

    Ported to Typescript

--- src/BareJS.ts
+++ src/BareJS.ts
@@ -10,233 +10,326 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-var BareJS = (function () {
-    function BareJS() {
+
+
+
+
+interface Window {
+    $: any|BareJS;
+    javascriptVersion: number;
+}
+
+interface Element {
+    /**
+     * Adds a class to an element
+     * @param clazz
+     *
+     * @see BareJS.addClass
+     */
+    addClass(clazz:string):void;
+
+    /**
+     * Removes a class from an element
+     * @param clazz
+     *
+     * @see BareJS.removeClass
+     */
+    removeClass(clazz:string):void;
+
+    /**
+     * Toggles a class
+     * @param clazz
+     * @param force
+     *
+     * @see BareJS.toggleClass
+     */
+    toggleClass(clazz:string, force:boolean):void;
+
+    hasClass(clazz:string):boolean;
+}
+
+
+class BareJS {
+    /**
+     * @type {Object}
+     */
+    protected static options = {};
+    /**
+     *
+     * @type {{version: string, shortcutEnabled: boolean, prototypesEnabled: boolean, javascriptVersion: number}}
+     */
+    protected static info: {
+        version: string,
+        shortcutEnabled: boolean,
+        prototypesEnabled: boolean,
+        javascriptVersion: number
+    } = {
+        version: '1.0@dev',
+        shortcutEnabled: false,
+        prototypesEnabled: false,
+        javascriptVersion: undefined
+    };
+
+    constructor() {
         "use strict";
+
         throw new Error('BareJS cann not be instantiated!');
     }
+
+
+
     /**
      * Get the Javascript version number
      * @returns {number}
      */
-    BareJS.getJavascriptVersion = function () {
+    public static getJavascriptVersion():number {
         "use strict";
+
         if (BareJS.info.javascriptVersion == null) {
-            var jsv = ["1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "2.0"];
-            var d = document;
-            for (var _i = 0, jsv_1 = jsv; _i < jsv_1.length; _i++) {
-                var version = jsv_1[_i];
-                var g = d.createElement('script'), s = BareJS.one('script');
+            let jsv: string[] = [ "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "2.0"];
+            let d = document;
+
+            for (let version of jsv) {
+                let g: Element = d.createElement('script'),
+                    s: Element = BareJS.one('script');
+
                 g.setAttribute("language", "JavaScript" + version);
                 g.nodeValue = "window.javascriptVersion=parseFloat('" + version + "');";
                 s.parentNode.insertBefore(g, s);
                 s.parentNode.removeChild(g);
             }
+
             BareJS.info.javascriptVersion = window.javascriptVersion;
             delete window.javascriptVersion;
         }
+
         return BareJS.info.javascriptVersion;
-    };
+    }
+
     /**
      * Activates the $ shortcut
      * @param {boolean} force if set to true, overrides the $ shortcut, even if it is already set (e.g. by another library like jQuery, etc...)
      * @returns {boolean} returns true if the shortcut is activated
      */
-    BareJS.enableShortcut = function (force) {
+    public static enableShortcut(force:boolean = false):boolean {
         "use strict";
-        if (force === void 0) { force = false; }
+
         if (typeof window.$ == "undefined" || force) {
             window.$ = BareJS;
+
             BareJS.info.shortcutEnabled = (window.$ == BareJS);
         }
+
         return BareJS.info.shortcutEnabled;
-    };
+    }
+
     /**
      * Adds prototypes to some objects (e.g. addClass etc... on Element)
      * @returns {boolean}
      */
-    BareJS.enablePrototypes = function () {
+    public static enablePrototypes():boolean {
         "use strict";
-        Element.prototype.addClass = function (clazz) {
+
+        Element.prototype.addClass = function (clazz:string) {
             BareJS.addClass(clazz, this);
         };
-        Element.prototype.removeClass = function (clazz) {
+
+        Element.prototype.removeClass = function (clazz:string) {
             BareJS.removeClass(clazz, this);
         };
-        Element.prototype.toggleClass = function (clazz, force) {
-            if (force === void 0) { force = false; }
+
+        Element.prototype.toggleClass = function (clazz:string, force:boolean = false) {
             BareJS.toggleClass(clazz, this, force);
         };
-        Element.prototype.hasClass = function (clazz) {
-            return BareJS.hasClass(clazz, this);
+
+        Element.prototype.hasClass = function (clazz:string) {
+            return <boolean>BareJS.hasClass(clazz, this);
         };
+
         // todo add Node.getElement and Node.getElements
         // todo add addClass etc... to NodeList etc...
+
         return BareJS.info.prototypesEnabled = true;
-    };
+    }
+
     /**
      *
      * @param {string} selector
      * @param {Element|Document} context
      * @returns {NodeList}
      */
-    BareJS.all = function (selector, context) {
+    public static all(selector:string, context:Element|Document = document):NodeListOf<Element> {
         "use strict";
-        if (context === void 0) { context = document; }
+
         return context.querySelectorAll(selector);
-    };
+    }
+
     /**
      *
      * @param {string} selector
      * @param {Element|Document} context
      * @returns {Element}
      */
-    BareJS.one = function (selector, context) {
+    public static one(selector:string, context:Element|Document = document):Element {
         "use strict";
-        if (context === void 0) { context = document; }
+
         return context.querySelector(selector);
-    };
+    }
+
+
     /**
      * @callback BareJs~iterateCallback
      * @param value
      * @param {number} index
      * @returns {boolean}
      */
+
+
     /**
      * Loops through an array and calls the provided callback function for every element
      * @param iterable
      * @param {BareJS~iterateCallback} cb
      */
-    BareJS.each = function (iterable, cb) {
+    public static each(iterable:any, cb:Function):void {
         "use strict";
-        var i = 0;
-        for (var _i = 0, iterable_1 = iterable; _i < iterable_1.length; _i++) {
-            var value = iterable_1[_i];
+
+        let i = 0;
+        for (let value of iterable) {
             cb(value, i++);
         }
-    };
+    }
+
     /**
      * Loops through an array and calls the provided callback function for every element, until the callback returns true for an element.
      * @param iterable
      * @param {BareJS~iterateCallback} cb
      * @returns {boolean} returns true, if the provided callback function has returned true for one element
      */
-    BareJS.some = function (iterable, cb) {
+    public static some(iterable:any, cb:Function):boolean {
         "use strict";
-        var i = 0;
-        for (var _i = 0, iterable_2 = iterable; _i < iterable_2.length; _i++) {
-            var value = iterable_2[_i];
+
+        let i = 0;
+        for (let value of iterable) {
             if (cb(value, i++)) {
                 return true;
             }
         }
+
         return false;
-    };
+    }
+
     /**
      *
      * @param {string} clazz
      * @param {Element|Element[]|NodeList} elements
      */
-    BareJS.addClass = function (clazz, elements) {
+    public static addClass(clazz:string, elements:Element|NodeListOf<Element>|Element[]):void {
         "use strict";
+
         if (elements.constructor === Element) {
-            elements = [elements];
+            elements = <Element[]>[elements];
         }
-        BareJS.each(elements, function (el) {
+
+        BareJS.each(elements, function (el:Element) {
             el.classList.add(clazz);
         });
-    };
+    }
+
     /**
      *
      * @param {string} clazz
      * @param {Element|Element[]|NodeList} elements
      */
-    BareJS.removeClass = function (clazz, elements) {
+    public static removeClass(clazz:string, elements:Element|NodeListOf<Element>|Element[]):void {
         "use strict";
+
         if (elements.constructor === Element) {
-            elements = [elements];
+            elements = <Element[]>[elements];
         }
-        BareJS.each(elements, function (el) {
+
+        BareJS.each(elements, function (el:Element) {
             el.classList.remove(clazz);
         });
-    };
+    }
+
     /**
      *
      * @param {string} clazz
      * @param {Element|Element[]|NodeList} elements
      * @param {boolean} force if set to true, the class will be added to the element, regardless if it is already present
      */
-    BareJS.toggleClass = function (clazz, elements, force) {
+    public static toggleClass(clazz:string, elements:Element|NodeListOf<Element>|Element[], force:boolean = false):void {
         "use strict";
-        if (force === void 0) { force = false; }
+
         if (elements.constructor === Element) {
-            elements = [elements];
+            elements = <Element[]>[elements];
         }
+
         if (force) {
             BareJS.addClass(clazz, elements);
-        }
-        else {
-            BareJS.each(elements, function (el) {
+        } else {
+            BareJS.each(elements, function (el:Element) {
                 el.classList.toggle(clazz);
             });
         }
-    };
+    }
+
     /**
      *
      * @param clazz
      * @param elements
      * @returns {boolean|boolean[]}
      */
-    BareJS.hasClass = function (clazz, elements) {
+    public static hasClass(clazz:string, elements:Element|NodeListOf<Element>|Element[]):boolean|boolean[] {
         "use strict";
+
         if (elements.constructor === Element) {
-            elements = [elements];
+            elements = <Element[]>[elements];
         }
-        var hasClass = [];
-        BareJS.each(elements, function (el) {
+
+        let hasClass:any[] = [];
+        BareJS.each(elements, function (el:Element) {
             hasClass.push(el.classList.contains(clazz));
         });
+
         if (hasClass.length > 1) {
             return hasClass;
-        }
-        else {
+        } else {
             return hasClass.shift();
         }
-    };
+    }
+
     /**
      *
      * @param options
      * @returns {Document|JSON|string}
      * @todo finish
      */
-    BareJS.request = function (options) {
-        "use strict";
-        if (options === void 0) { options = {}; }
-        var xmlhttp;
-        if (window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
+    public static request(
+        options:{
+
+        } = {
+
         }
-        else {
+    ):Document|JSON|string {
+        "use strict";
+
+        let xmlhttp:XMLHttpRequest;
+
+        if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else { // code for IE6, IE5
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
-        xmlhttp.open("GET", "ajax_info.txt", true);
+
+        xmlhttp.open("GET","ajax_info.txt",true);
         xmlhttp.send();
+
         return xmlhttp.responseXML;
-    };
-    /**
-     * @type {Object}
-     */
-    BareJS.options = {};
-    /**
-     *
-     * @type {{version: string, shortcutEnabled: boolean, prototypesEnabled: boolean, javascriptVersion: number}}
-     */
-    BareJS.info = {
-        version: '1.0@dev',
-        shortcutEnabled: false,
-        prototypesEnabled: false,
-        javascriptVersion: undefined
-    };
-    return BareJS;
-}());
-//# sourceMappingURL=BareJS.js.map
+    }
+
+    // todo add event functions (addEvent, fireEvent, removeEvent) & maybe something like the mootools pseudoevents (:relay() would be nice)
+    // todo add string formatting method
+    // todo add DOM traversing methodes (like getParent([selector]) etc...)
+    // todo methods for creating events
+}
