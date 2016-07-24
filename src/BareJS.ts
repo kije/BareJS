174b7f6f95b7c368b3ac08d30f6f4b20b0704953
kije <git@kije.ch>
2016-07-25 00:10:53 +0200

    changed project structure / some performance improvements

--- src/BareJS.ts
+++ src/BareJS.ts
@@ -11,43 +11,44 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+declare global {
+    interface Window {
+        $:any;
+        $$:any;
+        javascriptVersion:number;
+        XMLHttpRequest:XMLHttpRequest;
+    }
 
+    interface Element {
+        /**
+         * Adds a class to an element
+         * @param clazz
+         *
+         * @see BareJS.addClass
+         */
+        addClass(clazz:string):void;
 
+        /**
+         * Removes a class from an element
+         * @param clazz
+         *
+         * @see BareJS.removeClass
+         */
+        removeClass(clazz:string):void;
 
-interface Window {
-    $: any;
-    $$: any;
-    javascriptVersion: number;
+        /**
+         * Toggles a class
+         * @param clazz
+         * @param force
+         *
+         * @see BareJS.toggleClass
+         */
+        toggleClass(clazz:string, force:boolean):void;
+
+        hasClass(clazz:string):boolean;
+    }
 }
 
-interface Element {
-    /**
-     * Adds a class to an element
-     * @param clazz
-     *
-     * @see BareJS.addClass
-     */
-    addClass(clazz:string):void;
-
-    /**
-     * Removes a class from an element
-     * @param clazz
-     *
-     * @see BareJS.removeClass
-     */
-    removeClass(clazz:string):void;
-
-    /**
-     * Toggles a class
-     * @param clazz
-     * @param force
-     *
-     * @see BareJS.toggleClass
-     */
-    toggleClass(clazz:string, force:boolean):void;
-
-    hasClass(clazz:string):boolean;
-}
 
 
 class BareJS {
@@ -59,11 +60,11 @@ class BareJS {
      *
      * @type {{version: string, shortcutEnabled: boolean, prototypesEnabled: boolean, javascriptVersion: number}}
      */
-    protected static info: {
-        version: string,
-        shortcutEnabled: boolean,
-        prototypesEnabled: boolean,
-        javascriptVersion: number
+    protected static info:{
+        version:string,
+        shortcutEnabled:boolean,
+        prototypesEnabled:boolean,
+        javascriptVersion:number
     } = {
         version: '1.0@dev',
         shortcutEnabled: false,
@@ -78,7 +79,6 @@ class BareJS {
     }
 
 
-
     /**
      * Get the Javascript version number
      * @returns {number}
@@ -87,12 +87,12 @@ class BareJS {
         "use strict";
 
         if (BareJS.info.javascriptVersion == null) {
-            let jsv: string[] = [ "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "2.0"];
+            let jsv:string[] = ["1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "2.0"];
             let d = document;
 
             for (let version of jsv) {
-                let g: Element = d.createElement('script'),
-                    s: Element = BareJS.one('script');
+                let g:Element = d.createElement('script'),
+                    s:Element = BareJS.one('script');
 
                 g.setAttribute("language", "JavaScript" + version);
                 g.nodeValue = "window.javascriptVersion=parseFloat('" + version + "');";
@@ -166,6 +166,20 @@ class BareJS {
         return context.querySelectorAll(selector);
     }
 
+    private static isSimpleSelector(selector:string):boolean {
+        "use strict";
+
+        // check, if selector is simple (e.g. only contains an id or a tag name)
+        return (
+            selector.trim().indexOf(" ", 1) === -1 &&
+            selector.indexOf("[", 1) === -1 &&
+            selector.indexOf(":", 1) === -1 &&
+            selector.indexOf("*", 1) === -1 &&
+            selector.indexOf("#", 1) === -1 &&
+            selector.indexOf(".", 1) === -1
+        );
+    }
+
     /**
      *
      * @param {string} selector
@@ -174,6 +188,22 @@ class BareJS {
      */
     public static one(selector:string, context:Element|Document = document):Element {
         "use strict";
+
+        selector = selector.trim();
+
+        if (BareJS.isSimpleSelector(selector)) {
+            let firstCharacter:string = selector.substr(0,1);
+
+            if (firstCharacter === "#" && context == document) {
+                return (<Document>context).getElementById(selector.substr(1));
+            } else if (firstCharacter === ".") {
+                let results = context.getElementsByClassName(selector.substr(1));
+                return results.length > 0 ? results[0] : null;
+            } else if (["[", ":", "*"].indexOf(firstCharacter) === -1) {
+                let results = context.getElementsByTagName(selector);
+                return results.length > 0 ? results[0] : null;
+            }
+        }
 
         return context.querySelector(selector);
     }
@@ -228,9 +258,11 @@ class BareJS {
     public static addClass(clazz:string, elements:Element|NodeListOf<Element>|Element[]):void {
         "use strict";
 
-        if (elements.constructor === Element) {
+        if ("length" in elements && typeof (<Element[]>elements).length === "undefined") {
             elements = <Element[]>[elements];
         }
+        console.log(elements)
+
 
         BareJS.each(elements, function (el:Element) {
             el.classList.add(clazz);
@@ -245,7 +277,7 @@ class BareJS {
     public static removeClass(clazz:string, elements:Element|NodeListOf<Element>|Element[]):void {
         "use strict";
 
-        if (elements.constructor === Element) {
+        if ("length" in elements) {
             elements = <Element[]>[elements];
         }
 
@@ -263,7 +295,7 @@ class BareJS {
     public static toggleClass(clazz:string, elements:Element|NodeListOf<Element>|Element[], force:boolean = false):void {
         "use strict";
 
-        if (elements.constructor === Element) {
+        if ("length" in elements) {
             elements = <Element[]>[elements];
         }
 
@@ -285,7 +317,7 @@ class BareJS {
     public static hasClass(clazz:string, elements:Element|NodeListOf<Element>|Element[]):boolean|boolean[] {
         "use strict";
 
-        if (elements.constructor === Element) {
+        if ("length" in elements) {
             elements = <Element[]>[elements];
         }
 
@@ -307,13 +339,9 @@ class BareJS {
      * @returns {Document|JSON|string}
      * @todo finish
      */
-    public static request(
-        options:{
+    public static request(options:{
 
-        } = {
-
-        }
-    ):Document|JSON|string {
+    } = {}):Document|JSON|string {
         "use strict";
 
         let xmlhttp:XMLHttpRequest;
@@ -324,7 +352,7 @@ class BareJS {
             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
 
-        xmlhttp.open("GET","ajax_info.txt",true);
+        xmlhttp.open("GET", "ajax_info.txt", true);
         xmlhttp.send();
 
         return xmlhttp.responseXML;
@@ -335,3 +363,6 @@ class BareJS {
     // todo add DOM traversing methodes (like getParent([selector]) etc...)
     // todo methods for creating events
 }
+
+
+export = BareJS;
